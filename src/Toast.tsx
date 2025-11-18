c412506af94d45358855e18fd1d4e5e5a416f8cb
lucyandlucky <lucykfc1@gmail.com>
2025-11-18 16:37:43 +0800

    feat: add setRef & remove example folder

--- src/Toast.tsx
+++ src/Toast.tsx
@@ -1,11 +1,55 @@
-import type React from 'react';
+import React from 'react';
 import { LoggerProvider } from './context/logger-context';
 import { View } from 'react-native';
+import type { ToastRef, ToastProps } from './types';
 
-export function Toast(): React.ReactElement {
+const ToastRoot = React.forwardRef<ToastRef, ToastProps>((_props, ref) => {
+  React.useImperativeHandle(ref, () => ({
+    show() {},
+    hide() {},
+  }));
+
+  return <View />;
+});
+
+type ToastRefObj = {
+  current: ToastRef | null;
+};
+
+let refs: ToastRefObj[] = [];
+
+/**
+ * Adds a ref to the end of refs array
+ * @param newRef
+ */
+function addNewRef(newRef: ToastRef) {
+  refs.push({ current: newRef });
+  console.log('refs', refs);
+}
+
+/**
+ * Removes the passed-in ref from the file-level refs array
+ * @param oldRef
+ */
+function removeOldRef(oldRef: ToastRef | null) {
+  refs = refs.filter((r) => r.current !== oldRef);
+}
+
+export function Toast(props: ToastProps): React.ReactElement {
+  const toastRef = React.useRef<ToastRef | null>(null);
+
+  const setRef = React.useCallback((ref: ToastRef | null) => {
+    if (ref) {
+      toastRef.current = ref;
+      addNewRef(ref);
+    } else {
+      removeOldRef(toastRef.current);
+    }
+  }, []);
+
   return (
     <LoggerProvider enableLogs={false}>
-      <View />
+      <ToastRoot ref={setRef} {...props} />
     </LoggerProvider>
   );
 }
